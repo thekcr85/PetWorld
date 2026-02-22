b76d0cf70a82a98d0b0befe6ea8194c628a48417
thekcr85 <thekcr85@gmail.com>
2026-02-22 16:54:45 +0100

    Add ApplicationServiceExtensions to register application services

---
+++ src/PetWorld.Application/Extensions/ApplicationServiceExtensions.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using PetWorld.Application.Interfaces.Services;
+using PetWorld.Application.Services;
+using PetWorld.Domain.Interfaces.Services;
+
+namespace PetWorld.Application.Extensions;
+
+public static class ApplicationServiceExtensions
+{
+	public static IServiceCollection AddApplicationServices(this IServiceCollection services)
+	{
+		services.AddScoped<IChatService, ChatService>();
+
+		return services;
+	}
+}
