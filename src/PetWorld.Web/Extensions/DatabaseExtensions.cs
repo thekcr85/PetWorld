65370b84f95c8c292d26d2112a20867032787e4f
thekcr85 <thekcr85@gmail.com>
2026-02-22 17:03:22 +0100

    Add DatabaseExtensions to handle database initialization and seeding

---
+++ src/PetWorld.Web/Extensions/DatabaseExtensions.cs
@@ -0,0 +1,16 @@
+﻿using PetWorld.Infrastructure.Data;
+
+namespace PetWorld.Infrastructure.Extensions;
+
+public static class DatabaseExtensions
+{
+	public static async Task InitialiseDatabaseAsync(this WebApplication app)
+	{
+		using var scope = app.Services.CreateScope();
+
+		var context = scope.ServiceProvider.GetRequiredService<PetWorldDbContext>();
+
+		await context.Database.EnsureCreatedAsync();
+		await DbInitializer.SeedDataAsync(context);
+	}
+}
