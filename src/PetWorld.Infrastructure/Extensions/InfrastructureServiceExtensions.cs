29a1de24df3dda279ff35c56ce4114c18bb8a7a5
thekcr85 <thekcr85@gmail.com>
2026-02-22 16:55:05 +0100

    Add InfrastructureServiceExtensions to register infrastructure services

---
+++ src/PetWorld.Infrastructure/Extensions/InfrastructureServiceExtensions.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using PetWorld.Application.Interfaces.Repositories;
+using PetWorld.Application.Interfaces.Services;
+using PetWorld.Infrastructure.Data;
+using PetWorld.Infrastructure.Repositories;
+using PetWorld.Infrastructure.Services;
+
+namespace PetWorld.Infrastructure.Extensions;
+
+public static class InfrastructureServiceExtensions
+{
+	public static IServiceCollection AddInfrastructureServices(
+		this IServiceCollection services,
+		IConfiguration configuration)
+	{
+		var connectionString = configuration.GetConnectionString("DefaultConnection")
+			?? throw new InvalidOperationException("Connection string 'DefaultConnection' not found.");
+
+		services.AddDbContext<PetWorldDbContext>(options =>
+			options.UseMySql(connectionString, ServerVersion.AutoDetect(connectionString)));
+
+		services.AddScoped<IProductRepository, ProductRepository>();
+		services.AddScoped<IChatHistoryRepository, ChatHistoryRepository>();
+		services.AddScoped<IAiChatService, AiChatService>();
+
+		return services;
+	}
+}
